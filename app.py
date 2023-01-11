e1c5dc4eed46bae551b0dc457b8460f2c43d24af
Hannah Greven <greven.hannah@gmail.com>
2023-01-11 11:03:12 +0100

    Fix map location

--- app.py
+++ app.py
@@ -22,9 +22,8 @@ def init():
     add_adapters(model)
 
     print("loading model to CPU...")
-    print("device:", device)
-    checkpoint = torch.hub.load_state_dict_from_url('https://h2858852.stratoserver.net/snipaid/gptj-title-teaser-10k.pt')
-    model.load_state_dict(checkpoint, map_location=torch.device(device))
+    checkpoint = torch.hub.load_state_dict_from_url('https://h2858852.stratoserver.net/snipaid/gptj-title-teaser-10k.pt', map_location=torch.device('cpu'))
+    model.load_state_dict(checkpoint)
     print("done")
 
     # conditionally load to GPU
